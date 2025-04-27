599e4e9a06df30227fb12b82844a949c1532614a
Joe Turner <joe@agavemountain.com>
2025-04-27 14:13:54 -0500

    update doxygen comments

---
+++ lib/include/pentaledger/transportation/transportation.hpp
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: GPL-2.0-only
+//! \file transportation.hpp
+//!
+//! \copyright
+//!
+//! This file is part of the PentaLedger distribution.
+//! Copyright (c) 2025 Joe Turner.
+//!
+//! This program is free software: you can redistribute it and/or modify
+//! it under the terms of the GNU General Public License as published by
+//! the Free Software Foundation, version 3.
+//!
+//! This program is distributed in the hope that it will be useful, but
+//! WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//! General Public License for more details.
+//!
+//! You should have received a copy of the GNU General Public License
+//! along with this program. If not, see <http://www.gnu.org/licenses/>.
+#ifndef _TRANSPORTATION_HPP_
+#define _TRANSPORTATION_HPP_
+
+/** @defgroup transportation Transportation Module
+ *  @brief The PentaLedger shared library provides a set of objects and classes that
+ *  encapsulate typical financial accounting concepts.
+ *  @{
+ */
+
+/** @defgroup vehicle Mileage Records and Vehicle Expenses
+ *  @brief Classes and functions for managing vehicle mileage and expenses
+ *  @ingroup transportation
+ *  @{
+ */
+
+namespace pentaledger
+{
+    //! \brief Transportation namespace
+    //!
+    //! This namespace contains all the classes and functions related to transportation.
+    //!
+    //! \todo: Add a class for transportation.
+    //! \todo: Add a class for fuel efficiency.
+    //! \todo: Add a class for fuel types.
+    //! \todo: Add a class for fuel prices. 
+    namespace transportation
+    {
+
+    }
+}
+/** @} */ // End of vehicle group
+/** @} */ // End of transportation group
+
+#endif
