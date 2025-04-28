aa20d7718a392069da09e489df8cdd8702dbc8b3
Joe Turner <joe@agavemountain.com>
2025-04-28 09:44:44 -0500

    Stub in mileage rates with todo

--- lib/include/pentaledger/Doxyments/MainPage.hpp
+++ lib/include/pentaledger/Doxyments/MainPage.hpp
@@ -18,4 +18,42 @@ Topics:
 Questions, comments, concerns, or dissenting opinions should be directed 
 to joe.turner@infinity-surge.com
 
+  \section Categories
+ Click on the link(s) below on the description of some of the code:
+
+ - \ref designpatterns
+
+
+ \tableofcontents
+
+ \section Requirements
+
+ - Doxygen 1.8.0+ (for markdown support) and GraphViz.
+ - GNU GCC toolchain
+ - Git (obviously)
+ - CPPUNIT (for unit tests)
+
+\section make Building and Installing
+
+ \code
+ $ mkdir build
+ $ cmake ..
+ $ make; make install
+ \endcode
+
+ \section Generating Documentation
+
+ This project uses doxygen to generate the documentation.  To generate
+ this documentation, you must have doxygen installed.  Simple execute doxygen:
+ \code
+ $ make doc
+ \endcode
+
+ \section Running Unit Tests
+
+ You can run unit tests (if cppunit is installed on your development workstation):
+
+ \code
+ $ ctest -V
+ \endcode
 */
---
+++ lib/include/pentaledger/transportation/irs_mileage_rates.hpp
@@ -0,0 +1,38 @@
+//! \brief IRS Mileage Rates
+//! \file irs_mileage_rates.hpp
+//!
+//! \copyright
+//!
+//! This file is part of the ERPBeyond distribution.
+//! Copyright (c) 2023 Joe Turner.
+//!
+//! This program is free software: you can redistribute it and/or modify
+//! it under the terms of the GNU General Public License as published by
+//! the Free Software Foundation, version 3.
+//!
+//! This program is distributed in the hope that it will be useful, but
+//! WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//! General Public License for more details.
+//!
+//! You should have received a copy of the GNU General Public License
+//! along with this program. If not, see <http://www.gnu.org/licenses/>.
+#ifndef _IRS_MILEAGE_RATES_
+#define _IRS_MILEAGE_RATES_
+#include <time.h>
+
+namespace ioi
+{
+    namespace transportation
+    {
+
+        /** @addtogroup MILEAGE_RATES
+  
+    @{ */
+
+        //! \todo Implement IRS mileage rates
+
+    };
+};
+
+#endif
