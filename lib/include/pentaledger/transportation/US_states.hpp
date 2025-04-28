bd863cbc5e8eecaedcc28a235d0ada8c22579551
Joe Turner <joe@agavemountain.com>
2025-04-28 16:57:08 -0500

    Add us states

---
+++ lib/include/pentaledger/transportation/US_states.hpp
@@ -0,0 +1,86 @@
+//! \file US_states.hpp
+//! \author Joe Turner <joe@agavemountain.com>
+//!
+//! \copyright
+//! Copyright (c) 2023 Joe Turner.
+//!
+//! This program is free software: you can redistribute it and/or modify
+//! it under the terms of the GNU General Public License as published by
+//! the Free Software Foundation, version 3.
+//!
+//! This program is distributed in the hope that it will be useful, but
+//! WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//! General Public License for more details.
+//!
+//! You should have received a copy of the GNU General Public License
+//! along with this program. If not, see <http://www.gnu.org/licenses/>.
+#ifndef _US_STATES_HPP_
+#define _US_STATES_HPP_
+
+namespace pentaledger
+{
+    namespace transportation
+    {
+        enum eUS_STATE
+        {
+            AK, //!< Alaska
+            AL, //!< Alabama
+            AR, //!< Arkansas
+            AS, //!< American Samoa
+            AZ, //!< Arizona
+            CA, //!< California
+            CO, //!< Colorado
+            CT, //!< Connecticut
+            DC, //!< District of Columbia
+            DE, //!< Delaware
+            FL, //!< Florida
+            GA, //!< Georgia
+            GU, //!< Guam
+            HI, //!< Hawaii
+            IA, //!< Iowa
+            ID, //!< Idaho
+            IL, //!< Illinois
+            IN, //!< Indiana
+            KS, //!< Kansas
+            KY, //!< Kentucky
+            LA, //!< Louisiana
+            MA, //!< Massachusetts
+            MD, //!< Maryland
+            ME, //!< Maine
+            MI, //!< Michigan
+            MN, //!< Minnesota
+            MO, //!< Missouri
+            MS, //!< Mississippi
+            MT, //!< Montana
+            NC, //!< North Carolina
+            ND, //!< North Dakota
+            NE, //!< Nebraska
+            NH, //!< New Hampshire
+            NJ, //!< New Jersey
+            NM, //!< New Mexico
+            NV, //!< Nevada
+            NY, //!< New York
+            OH, //!< Ohio
+            OK, //!< Oklahoma
+            OR, //!< Oregon
+            PA, //!< Pennsylvania
+            PR, //!< Puerto Rico
+            RI, //!< Rhode Island
+            SC, //!< South Carolina
+            SD, //!< South Dakota
+            TN, //!< Tennessee
+            TX, //!< Texas
+            UT, //!< Utah
+            VA, //!< Virginia
+            VI, //!< Virgin Islands
+            VT, //!< Vermont
+            WA, //!< Washington
+            WI, //!< Wisconsin
+            WV, //!< West Virginia
+            WY, //!< Wyoming
+
+        };
+
+    }
+}
