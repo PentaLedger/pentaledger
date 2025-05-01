c779f8d699559e9a55f732f61b0669d01c279bb5
Joe Turner <joe@agavemountain.com>
2025-04-30 22:03:22 -0500

    Recovered from backup

---
+++ lib/include/pentaledger/amazon/amazon_fees.hpp
@@ -0,0 +1,30 @@
+//! \file namespaces.hpp
+//! \author Joe Turner <joe.turner@infinity-surge.com>
+//!
+//! \copyright
+//! Copyright (c) 2023 Infinity Surge Inc.
+//!
+//! This program is free software: you can redistribute it and/or modify
+//! it under the terms of the GNU General Public License as published by
+//! the Free Software Foundation, version 2.
+//!
+//! This program is distributed in the hope that it will be useful, but
+//! WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//! General Public License for more details.
+//!
+//! You should have received a copy of the GNU General Public License
+//! along with this program. If not, see <http://www.gnu.org/licenses/>.
+#ifndef _AMAZON_FEES_HPP_
+#define _AMAZON_FEES_HPP_
+
+//! \todo Implement Amazon listing fee calculations
+/* 
+Recovered from backup. 
+template<class T>
+T amz_listing_fee(T item_cost)
+{
+    
+}
+*/
+#endif
