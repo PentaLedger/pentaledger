68d2aa39f8e385234c71b90ed06de38e604b4706
Joe Turner <joe@agavemountain.com>
2025-05-02 13:54:00 -0500

    update contribution limits.

--- lib/src/accounting/tax/irs_contribution_limits.cpp
+++ lib/src/accounting/tax/irs_contribution_limits.cpp
@@ -39,20 +39,20 @@ namespace pentaledger
                 // The amount you can defer (including pre-tax and Roth contributions) to all your plans 
                 // (not including 457(b) plans) is $23,000 in 2024
                 {OVERALL_IRA_LIMIT, {2024, OVERALL_IRA_LIMIT, 23000, 30500}},
-                {IRA, {2024, IRA, 6500, 1000}},
-                {ROTH_IRA, {2024, ROTH_IRA, 6500, 1000}},
-                {SIMPLE_IRA, {2024, SIMPLE_IRA, 15500, 3000}},
-                {SEP_IRA, {2024, SEP_IRA, 66000, 7500}},
-                {R401K, {2024, R401K, 22500, 7500}},
+                {IRA, {2024, IRA, 7000, 8000}},
+                {ROTH_IRA, {2024, ROTH_IRA, 7000, 8000}},
+                {SIMPLE_IRA, {2024, SIMPLE_IRA, 16500, 19500}},
+                {SEP_IRA, {2024, SEP_IRA, 69000, 69000}},
+                {R401K, {2024, R401K, 23000, 30500}},
                 {OTHER, {2024, OTHER, 0.0, 0.0}} // Placeholder for other accounts
             }},
             {2023, {
-                {OVERALL_IRA_LIMIT, {2023, OVERALL_IRA_LIMIT, 23500, 31000}},
-                {IRA, {2023, IRA, 7000, 8000}},
-                {ROTH_IRA, {2023, ROTH_IRA, 6500, 1000}},
-                {SIMPLE_IRA, {2023, SIMPLE_IRA, 15500, 3000}},
+                {OVERALL_IRA_LIMIT, {2023, OVERALL_IRA_LIMIT, 22500, 30000}},
+                {IRA, {2023, IRA, 6500, 7500}},
+                {ROTH_IRA, {2023, ROTH_IRA, 6500, 7500}},
+                {SIMPLE_IRA, {2023, SIMPLE_IRA, 15500, 1900}},
                 {SEP_IRA, {2023, SEP_IRA, 66000, 7500}},
-                {R401K, {2023, R401K, 23500, 7500}},
+                {R401K, {2023, R401K, 22500, 30000}},
                 {OTHER, {2023, OTHER, 0.0, 0.0}} // Placeholder for other accounts
             }},
             // Add more years as needed
