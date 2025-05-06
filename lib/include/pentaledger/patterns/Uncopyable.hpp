e4f782db3ca48864de19f28614b37d4431397074
Joe Turner <joe@agavemountain.com>
2025-05-06 18:34:21 -0500

    Update namespace comment

--- lib/include/pentaledger/patterns/Uncopyable.hpp
+++ lib/include/pentaledger/patterns/Uncopyable.hpp
@@ -19,6 +19,17 @@
 
 namespace pentaledger
 {
+	/**
+ * \brief Design Patterns and Idioms
+ *
+ * A design pattern is a generalized reusable solution to a commonly occurring problem
+ * within a given software design context. Leveraging design patterns can
+ * speed up development by providing tested, proven development paradigms.
+ *
+ * While the design pattern fad may have passed, there are still some very useful
+ * design idioms out there.
+ *
+ */
 	namespace patterns
 	{
 
