15d920a2e8b0dc47d0e656038a1a7ee80faac206
Joe Turner <joe@agavemountain.com>
2025-06-25 13:41:06 -0500

    (task):add readParam helper

--- server/cmd/api/helpers.go
+++ server/cmd/api/helpers.go
@@ -16,21 +16,22 @@
 package main
 
 import (
-	"fmt"
+	"errors"
 	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-func (app *application) createCompanyHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new company")
-}
+// Retrieve the "id" URL parameter from the current request context, then convert it to
+// an integer and return it. If the operation isn't successful, return 0 and an error.
+func (app *application) readIDParam(r *http.Request) (int64, error) {
+	params := httprouter.ParamsFromContext(r.Context())
 
-func (app *application) showCompanyHandler(w http.ResponseWriter, r *http.Request) {
-
-	id, err := app.readIDParam(r)
-	if err != nil {
-		http.NotFound(w, r)
-		return
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		return 0, errors.New("invalid id parameter")
 	}
 
-	fmt.Fprintf(w, "show the details of Company %d\n", id)
+	return id, nil
 }
