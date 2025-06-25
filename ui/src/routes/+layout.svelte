79bc61191d702b66f94ae7ae55aaf8c6b7f3279a
Joe Turner <joe@agavemountain.com>
2025-06-24 22:50:09 -0500

    (feat): Add fontawesome-free

--- ui/src/routes/+layout.svelte
+++ ui/src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>  
     let { children } = $props();
     import "../app.css";
+    import '@fortawesome/fontawesome-free/css/all.min.css'
 </script>
 {@render children()}
--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <h1 class="text-3xl font-bold underline">
-    Hello world!
+    <i class="fa-regular fa-lightbulb">Hello world!</i>
 </h1>
 <style lang="postcss">  
     @reference "tailwindcss"; 
-     
+
     :global(html) {    
         background-color: 
         theme(--color-gray-100);  
